720b99f3b544f0ef69fee878eb99fb5c50290a3f
Asheesh Laroia <asheesh@asheesh.org>
2021-07-01 20:04:57 -0700

    Import sample code from bug

--- repro1277/src/repro1277/app.py
+++ repro1277/src/repro1277/app.py
@@ -5,22 +5,38 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
+TIMER_MIN=1
+TIMER_MAX=19
+
 
 class Repro1277(toga.App):
 
     def startup(self):
-        """
-        Construct and show the Toga application.
-
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
         main_box = toga.Box()
+
+        self.timer_label=toga.Label(text="Counter: ")
+        self.timer_label.style.width=200
+        self.timer_label.style.padding=5
+
+        self.timer_slide=toga.Slider(range=(TIMER_MIN,TIMER_MAX),tick_count=(TIMER_MAX-TIMER_MIN+1),on_change=(self.timer_change),
+                                     on_release=(self.timer_set))
+        self.timer_slide.style.padding=5
+        self.timer_slide.style.padding_top=5
+        self.timer_slide.style.width=200
+        self.timer_slide.value=10.0
+
+        main_box.add(self.timer_slide)
+        main_box.add(self.timer_label)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def timer_change(self,widget):
+        self.timer_label.text="Counter: "+str(self.timer_slide.tick_value)
+
+    def timer_set(self,widget):
+        pass
 
 
 def main():
